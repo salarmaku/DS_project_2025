d2087cb787b20a26fb31053064929426faf9ab63
Amirmahdi Rezazadeh <amirmahdirezazadeh10@gmail.com>
2026-01-05 10:14:38 +0330

    changed it so its now a class with methods

--- Queue/circular_queue/Isempty.py
+++ Queue/circular_queue/Isempty.py
@@ -1,9 +1,22 @@
-Queue = [1, 2, 3, 4, 5]
-empt = []
-def IsEmpty(array):
-    if Queue == empt:
-        return True
-    else:
-        return False
+class circularQueue():
+    def __init__(self, size):
+        self.size = size
+        self.queue = [None] * size
+        self.front = -1
+        self.rear = -1
 
-print(IsEmpty(Queue))
+    def isEmpty(self):
+        if self.front == -1 and self.rear == -1:
+            print("Yes")
+            return 1
+        else:
+            print("No")
+            return 0
+        
+queue = circularQueue(5)
+queue.isEmpty()
+
+queue.add = 2
+queue.front = 0
+queue.rear = 0
+queue.isEmpty()
