7244c84409bf59b6f859e72ff2c2c71ec99e8bb3
Amirmahdi Rezazadeh <amirmahdirezazadeh10@gmail.com>
2026-01-05 16:49:20 +0330

    changed it into a two-way linked list

--- LinkList/Two_way_linklist/Insert_at_End.py
+++ LinkList/Two_way_linklist/Insert_at_End.py
@@ -2,29 +2,38 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
+        self.prev = None
 
 class LinkedList:
     def __init__(self):
         self.head = None
+        self.end = None
 
     def insertAtEnd(self, data):
         newNode = Node(data)
 
         if self.head == None:
             self.head = newNode
+            self.end = newNode
             return
 
-        current = self.head
-        while current.next is not None:
-            current = current.next
+        node.prev = self.end
+        self.end.next = newNode
+        self.end = newNode
         
-        current.next = newNode
-
-    def display(self):
+    def displayForward(self):
         current = self.head
         while current is not None:
             print(current.data)
             current = current.next
+        print("None")
+
+    def displayBackward(self):
+        current = self.end
+
+        while current is not None:
+            print(current.data)
+            current = current.prev
         print("None")
 
 ll = LinkedList()
@@ -32,4 +41,5 @@ ll.insertAtEnd(1)
 ll.insertAtEnd(2)
 ll.insertAtEnd(3)
 
-ll.display()
+ll.displayForward()
+ll.displayBackward()
